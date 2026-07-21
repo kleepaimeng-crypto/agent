a875d7804d1c760aef1f237047a26036bad8ca92
Xlan <kleepaimeng@gmail.com>
2026-07-21 17:15:30 +0800

    补充资料

---
+++ agent-learning/04-labs/lab-01-llm-api/Fastapi_base/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
