85344ca9df3621c21d4d4ae0da6b9b2a43daeb01
kleepaimeng-crypto <kleepaimeng-crypto@users.noreply.github.com>
2026-07-27 16:49:40 +0800

    补充

--- agent-learning/04-labs/lab-01-llm-api/Fastapi_base/main.py
+++ agent-learning/04-labs/lab-01-llm-api/Fastapi_base/main.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
+@app.middleware("http")
+async def middleware1(request, call_next):
+    print("中间件1 start")
+
+    response = await call_next(request)
+
+    print("中间件1 end")
+    return response
+
+
 @app.get("/")
 async def root():
+    print("root 执行")
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
